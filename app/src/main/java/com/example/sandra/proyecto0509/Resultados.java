25517b7f2f13f83b68ac82d17de43453cf929df6
SandraDelgadoMartinez <manolovalverdejo@hotmail.com>
2019-03-13 18:15:19 +0000

    Creacion de una tabla para mostrar todos los meses con sus respectivos contadores totales

---
+++ app/src/main/java/com/example/sandra/proyecto0509/Resultados.java
@@ -0,0 +1,29 @@
+package com.example.sandra.proyecto0509;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Resultados extends AppCompatActivity {
+
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+
+
+
+    }
+
+}
