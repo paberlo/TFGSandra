bffd5adfea337d30cbcba14e8df1cf7f654ab9fe
SandraDelgadoMartinez <manolovalverdejo@hotmail.com>
2019-03-25 19:18:18 +0000

    Base de datos funcionando con exito

--- app/src/main/java/com/example/sandra/proyecto0509/PantallaSecundaria.java
+++ app/src/main/java/com/example/sandra/proyecto0509/PantallaSecundaria.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PantallaSecundaria extends AppCompatActivity
 {
     ImageView informacion;
     Button empezar;
     Button resultados;
+
+    TextView tv1,tv2,tv3,tv4;
 
 
     @Override
@@ -24,6 +27,24 @@ public class PantallaSecundaria extends AppCompatActivity
         empezar=findViewById(R.id.btn_empezar);
         informacion=findViewById(R.id.btn_informacion);
         resultados=findViewById(R.id.btn_resultados);
+
+        tv1=findViewById(R.id.tvnombre);
+        tv2=findViewById(R.id.tvusername);
+        tv3=findViewById(R.id.tvpassword);
+        tv4=findViewById(R.id.tvedad);
+
+        //recibimos los diferentes intents
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        String username=intent.getStringExtra("username");
+        String password=intent.getStringExtra("password");
+        int age=intent.getIntExtra("age",-1);
+
+        //asignamos los datos a nuestros textviews
+        tv1.setText(name);
+        tv2.setText(username);
+        tv3.setText(password);
+        tv4.setText(age+"");
 
 
         empezar.setOnClickListener(new View.OnClickListener()
