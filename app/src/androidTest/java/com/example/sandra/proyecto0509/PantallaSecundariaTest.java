37e5ed71c8ac6db1d156ba022acc079828d25d87
SandraDelgadoMartinez <manolovalverdejo@hotmail.com>
2019-08-30 18:14:44 +0200

    Test de usuarios y pantalla secundaria

--- app/src/androidTest/java/com/example/sandra/proyecto0509/PantallaSecundariaTest.java
+++ app/src/androidTest/java/com/example/sandra/proyecto0509/PantallaSecundariaTest.java
@@ -12,11 +12,16 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
-@RunWith(AndroidJUnit4.class) //lo tienes
+@RunWith(AndroidJUnit4.class)
 public class PantallaSecundariaTest {
+
+    //Comprobar que el btn_empezar esta visible para el usuario
     @Test
-   public void clickLoginButton_opensLoginUi() throws Exception{
+   public void clickLoginButton_opensLoginUiEmpezar() throws Exception{
         onView(withId(R.id.btn_empezar)).perform(click())               // click() is a ViewAction
-                .check(matches(isDisplayed()));                         //check(matches(isDisplayed())); //la prueba
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.btn_resultados)).perform(click())
+                .check(matches(isDisplayed()));//check(matches(isDisplayed())); //la prueba
     }
+
 }
