b54cf1c377e3761684c388f3c13950e82d89f53a
SandraDelgadoMartinez <manolovalverdejo@hotmail.com>
2019-09-23 09:28:09 +0200

    Test de usuarios y pantalla secundaria tick verde

--- app/src/androidTest/java/com/example/sandra/proyecto0509/MainActivityTest.java
+++ app/src/androidTest/java/com/example/sandra/proyecto0509/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.example.sandra.proyecto0509;
 
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
@@ -15,6 +16,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity>activityActivityTestRule= new ActivityTestRule<MainActivity>(MainActivity.class);
 
     @Test
     public void clickLoginButton_opensLoginUi() throws Exception{
--- app/src/androidTest/java/com/example/sandra/proyecto0509/PantallaSecundariaTest.java
+++ app/src/androidTest/java/com/example/sandra/proyecto0509/PantallaSecundariaTest.java
@@ -1,12 +1,13 @@
 package com.example.sandra.proyecto0509;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -15,13 +16,11 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class PantallaSecundariaTest {
 
-    //Comprobar que el btn_empezar esta visible para el usuario
-    @Test
-   public void clickLoginButton_opensLoginUiEmpezar() throws Exception{
-        onView(withId(R.id.btn_empezar)).perform(click())               // click() is a ViewAction
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.btn_resultados)).perform(click())
-                .check(matches(isDisplayed()));//check(matches(isDisplayed())); //la prueba
-    }
+    @Rule
+    public ActivityTestRule<PantallaSecundaria> activityTestRule= new ActivityTestRule<PantallaSecundaria>(PantallaSecundaria.class);
 
+    @Test
+    public void mostrarBotones(){
+        onView(withId(R.id.btn_empezar)).check(matches(isDisplayed()));
+    }
 }
