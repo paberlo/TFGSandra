bb4ae3b0b000532687f48bde066820533cc6bbe0
SandraDelgadoMartinez <manolovalverdejo@hotmail.com>
2019-11-08 13:27:00 +0100

    Modificacion testMainActivity

--- app/src/androidTest/java/com/example/sandra/proyecto0509/MainActivityTest.java
+++ app/src/androidTest/java/com/example/sandra/proyecto0509/MainActivityTest.java
@@ -1,7 +1,15 @@
 package com.example.sandra.proyecto0509;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,12 +17,14 @@ import org.junit.runner.RunWith;
 
 import java.util.Random;
 
+import androidx.annotation.NonNull;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.CoordinatesProvider;
 import androidx.test.espresso.action.GeneralClickAction;
 import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Tap;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
@@ -27,10 +37,15 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest{
+public class MainActivityTest extends LoginTest {
+
+    private String username="conchita2@hotmail.com";
+    private String password="conchita";
 
     @Rule
     public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    /*@Rule
+    public IntentsTestRule<CrearUsuarios> activityRule = new IntentsTestRule<>(CrearUsuarios.class);*/
 
     /*Test que comprueba que el boton star y brillo son visibles
      * y al hacer click realizan su funcion correspondiente*/
@@ -46,10 +61,19 @@ public class MainActivityTest{
 
     /*Test que comprueba la visilibilidad de los botones mas y menos
      * y su funcionalidad al hacer click*/
+
     @Test
     public void btn_masymenos() {
-        onView(withId(R.id.btn_mas)).perform(click()).check(matches(isDisplayed()));
-        onView(withId(R.id.btn_menos)).perform(click()).check(matches(isDisplayed()));
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    onView(withId(R.id.btn_mas)).perform(click()).check(matches(isDisplayed()));
+                    onView(withId(R.id.btn_menos)).perform(click()).check(matches(isDisplayed()));
+                }
+            }
+        });
+
     }
 
     /*Test que comprueba que el seekbar llega al limite correspondiente*/
