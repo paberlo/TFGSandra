1e4d0622ee3560cc941a5ca80552179deb6997db
SandraDelgadoMartinez <manolovalverdejo@hotmail.com>
2019-11-11 17:09:07 +0100

    mainactivitytest no botonmasymenos

--- app/src/test/java/com/example/sandra/proyecto0509/ExampleUnitTest.java
+++ app/src/test/java/com/example/sandra/proyecto0509/ExampleUnitTest.java
@@ -1,26 +1,32 @@
 package com.example.sandra.proyecto0509;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
 
     private String username="conchita2@hotmail.com";
     private String password="conchita";
 
-    private final String FAKE_STRING="Sesion iniciada con email " + username;
+    final String FAKE_STRING="Sesion iniciada con email " + username;
+
     @Test
     public void addition_isCorrect() {
 
-
-        //CrearUsuarios aux= new CrearUsuarios();
-        //assertEquals(aux.iniciarsesion("conchita2@hotmail.com", "conchita"));
     }
 }
